ec7639f53357907f6a43563a9af6f50e14a6fa1a
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-26 17:08:03 -0700

    authentication page is functional and will be starting

--- auth/auth.js
+++ auth/auth.js
@@ -1,6 +1,7 @@
-import { checkAuth, redirectIfLoggedIn, signInUser, getUser } from '../auth-utils.js';
+import { checkAuth, redirectIfLoggedIn, signInUser, signUpUser, getUser } from '../auth-utils.js';
 
 const signInForm = document.getElementById('sign-in');
+const signUpForm = document.getElementById('sign-up');
 
 window.addEventListener('load', async () => {
     await checkAuth();
@@ -10,6 +11,13 @@ signInForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const formData = new FormData(signInForm);
     await signInUser(formData.get('email'), formData.get('password'));
+    redirect();
+});
+
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(signUpForm);
+    await signUpUser(formData.get('email'), formData.get('password'));
     redirect();
 });
 
