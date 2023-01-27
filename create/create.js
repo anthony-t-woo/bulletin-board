668d725a4f704311ecc19b2e8fe72b08db305b80
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-27 11:58:48 -0700

    random background color selected in render-utils

--- create/create.js
+++ create/create.js
@@ -1,9 +1,12 @@
 import { checkAuthFromCreate } from '../auth-utils.js';
 import { addPost } from '../fetch-utils.js';
+import { renderPost } from '../render-utils.js';
 
 const createPostForm = document.getElementById('create-post');
 const homeButton = document.getElementById('home');
-
+const previewContainerEl = document.getElementById('preview-container');
+const inputsEl = document.querySelectorAll('input');
+console.log(inputsEl);
 window.addEventListener('load', async () => {
     await checkAuthFromCreate();
 });
@@ -17,4 +20,18 @@ createPostForm.addEventListener('submit', async (e) => {
 
 homeButton.addEventListener('click', () => {
     location.replace('../');
+});
+
+createPostForm.addEventListener('keyup', () => {
+    console.log('change detected');
+    const formData = new FormData(createPostForm);
+    let previewData = {
+        topic: formData.get('topic'),
+        message: formData.get('message'),
+        contact: formData.get('contact'),
+    };
+
+    const previewEl = renderPost(previewData);
+    previewContainerEl.textContent = '';
+    previewContainerEl.append(previewEl);
 });
--- render-utils.js
+++ render-utils.js
@@ -3,8 +3,13 @@ export function renderPost(post) {
     const topicEl = document.createElement('h2');
     const messageEl = document.createElement('p');
     const contactEl = document.createElement('p');
-
+    const colors = ['yellow', 'greenyellow', 'hotpink', 'lavender', 'skyblue'];
     div.classList.add('post-card');
+    if (post.background) {
+        div.style.background = post.background;
+    } else {
+        div.style.background = colors[Math.floor(Math.random() * colors.length)];
+    }
 
     topicEl.textContent = post.topic;
     messageEl.textContent = post.message;
