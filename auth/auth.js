4be92b5128102f1ba2c64f58c4be381570708322
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-26 19:18:15 -0700

    added home button to create page and to auth page

--- auth/auth.js
+++ auth/auth.js
@@ -2,6 +2,7 @@ import { checkAuth, redirectIfLoggedIn, signInUser, signUpUser, getUser } from '
 
 const signInForm = document.getElementById('sign-in');
 const signUpForm = document.getElementById('sign-up');
+const homeButton = document.getElementById('home');
 
 window.addEventListener('load', async () => {
     await checkAuth();
@@ -27,3 +28,6 @@ async function redirect() {
         redirectIfLoggedIn();
     }
 }
+homeButton.addEventListener('click', () => {
+    location.replace('../');
+});
--- create/create.js
+++ create/create.js
@@ -2,6 +2,7 @@ import { checkAuthFromCreate } from '../auth-utils.js';
 import { addPost } from '../fetch-utils.js';
 
 const createPostForm = document.getElementById('create-post');
+const homeButton = document.getElementById('home');
 
 window.addEventListener('load', async () => {
     await checkAuthFromCreate();
@@ -11,5 +12,9 @@ createPostForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const formData = new FormData(createPostForm);
     await addPost(formData.get('topic'), formData.get('message'), formData.get('contact'));
+    location.replace('../');
+});
+
+homeButton.addEventListener('click', () => {
     location.replace('../');
 });
